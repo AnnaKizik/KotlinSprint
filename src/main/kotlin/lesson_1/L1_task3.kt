a0be57a73ef4ea388721f00435a1e84bf29a657e
annakizik <a.v.kizik@gmail.com>
2025-04-27 19:38:32 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/L1_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main(){
+
+    val year = 1961
+    var hour = 10
+    var minute = 55
+
+    println("год полета Юрия Гагарина в космос: $year")
+//    println("час взлета: $hour")
+//    println("минута взлета: $minute")
+
+//    println("час посадки: $hour")
+//    println("минута посадки: $minute")
+
+    println("Время посадки: $hour:$minute")
+
+
+}
