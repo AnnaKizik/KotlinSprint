1860ab364a4b2dc6f166d3e34aedefc0ac8903c3
annakizik <a.v.kizik@gmail.com>
2025-04-27 20:33:32 +0300

    KS-1-3 внесла правки

--- src/main/kotlin/lesson_1/L1_task3.kt
+++ src/main/kotlin/lesson_1/L1_task3.kt
@@ -1,19 +1,21 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
 
     val year = 1961
-    var hour = 10
-    var minute = 55
+    var hour = "09"
+    var minute = "07"
 
     println("год полета Юрия Гагарина в космос: $year")
+
 //    println("час взлета: $hour")
 //    println("минута взлета: $minute")
+    println("Время взлета: $hour:$minute")
 
+    hour = "10"
+    minute = "55"
 //    println("час посадки: $hour")
 //    println("минута посадки: $minute")
-
     println("Время посадки: $hour:$minute")
-
 
 }
