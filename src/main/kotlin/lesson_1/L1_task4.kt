376d20eb58e0c74d306f66e7f07459332bc6bde8
annakizik <a.v.kizik@gmail.com>
2025-04-28 19:31:15 +0300

    KS-1-4

--- src/main/kotlin/lesson_1/L1_task4.kt
+++ src/main/kotlin/lesson_1/L1_task4.kt
@@ -1,16 +1,19 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
 
-    val length: Long = 40_868_600_000
-    val age:Byte = 27
+    val length: Long = 40_868_600_000L
+    val age: Byte = 27
     val partOfDay: Float = 0.075f
-    val seconds:Int = 6480
-    val partOfYear:Float = 2.0547945205479453E-4f
+    val seconds: Short = 6480
+    val partOfYear: Float = 2.0547945205479453E-4f
     val apogee: Int = 327000
 
-
-
-
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
 
 }
