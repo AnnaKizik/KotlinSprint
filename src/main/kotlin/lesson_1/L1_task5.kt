01c33c231bb619e649ecf31b18f4bdbdcdc66559
annakizik <a.v.kizik@gmail.com>
2025-04-28 19:00:34 +0300

    KS-1-5 создана константа, изменена запись вывода времени по формату

--- src/main/kotlin/lesson_1/L1_task5.kt
+++ src/main/kotlin/lesson_1/L1_task5.kt
@@ -2,13 +2,14 @@ package org.example.lesson_1
 
 fun main() {
 
+    const val numberOfTimeUnits = 60
     val seconds = 6480
-    val formatSeconds = seconds % 60
-    val minutes: Int = seconds / 60
-    val formatMinutes = minutes % 60
-    val hours: Int = minutes / 60
+    val formatSeconds = seconds % numberOfTimeUnits
+    val minutes: Int = seconds / numberOfTimeUnits
+    val formatMinutes = minutes % numberOfTimeUnits
+    val hours: Int = minutes / numberOfTimeUnits
 
-    val timeInFormat = String.format("%02d:%02d:%02d", hours, formatMinutes, formatSeconds)
+    val timeInFormat = "%02d:%02d:%02d".format(hours, formatMinutes, formatSeconds)
 
     println("Время в полете: $timeInFormat")
 
