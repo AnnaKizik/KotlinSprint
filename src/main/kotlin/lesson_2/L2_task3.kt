881c031d0ea61ef9e94f241b4c57263e237a360c
annakizik <a.v.kizik@gmail.com>
2025-04-29 21:10:59 +0300

    KS-2-3

--- src/main/kotlin/lesson_2/L2_task3.kt
+++ src/main/kotlin/lesson_2/L2_task3.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
+const val TIME_UNIT = 60
+
 fun main() {
 
     val departureTimeHour: Int = 9
     val departureTimeMinute: Int = 39
     val travelTime: Int = 457
-    val timeUnit = 60
 
-    val arrivalTimeHour: Int = departureTimeHour + (departureTimeMinute + travelTime) / timeUnit
-    val arrivalTimeMinute: Int = (departureTimeMinute + travelTime) % timeUnit
+    val arrivalTimeHour: Int = departureTimeHour + (departureTimeMinute + travelTime) / TIME_UNIT
+    val arrivalTimeMinute: Int = (departureTimeMinute + travelTime) % TIME_UNIT
 
     println("Время прибытия поезда: $arrivalTimeHour:$arrivalTimeMinute")
 
