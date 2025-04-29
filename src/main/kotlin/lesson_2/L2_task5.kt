fb1b061be174315282e216535b348367c27f49c0
annakizik <a.v.kizik@gmail.com>
2025-04-29 20:13:11 +0300

    KS-2-5 Значение ставки теперь в %

--- src/main/kotlin/lesson_2/L2_task5.kt
+++ src/main/kotlin/lesson_2/L2_task5.kt
@@ -5,12 +5,11 @@ import kotlin.math.pow
 fun main() {
 
     val enteredSum = 70_000
-    val interestRate = 0.167
+    val interestRate = 16.7
     val annualAccruals = 1
     val depositPeriod = 20
 
-
-    val futureDeposit = enteredSum * (1 + interestRate / annualAccruals).pow(annualAccruals * depositPeriod)
+    val futureDeposit = enteredSum * (1 + interestRate * 0.01 / annualAccruals).pow(annualAccruals * depositPeriod)
 
     println("Размер вклада через 20 лет составит: ${"%.3f".format(futureDeposit)}")
 
