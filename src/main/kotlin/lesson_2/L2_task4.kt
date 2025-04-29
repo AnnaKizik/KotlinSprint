44414e87ea4dfae7efc186ab427e01c635185ded
annakizik <a.v.kizik@gmail.com>
2025-04-29 20:26:15 +0300

    KS-2-4 Значение процента – целое число

--- src/main/kotlin/lesson_2/L2_task4.kt
+++ src/main/kotlin/lesson_2/L2_task4.kt
@@ -4,10 +4,11 @@ fun main() {
 
     val amountOfCrystalOreNoBuff = 7
     val amountOfIronOreNoBuff = 11
-    val bonus = 0.2
+    val bonus = 20
+    val intoProportion = 0.01
 
-    val crystalOreBonus = (amountOfCrystalOreNoBuff * bonus).toInt()
-    val ironOreBonus = (amountOfIronOreNoBuff * bonus).toInt()
+    val crystalOreBonus = (amountOfCrystalOreNoBuff * bonus * intoProportion).toInt()
+    val ironOreBonus = (amountOfIronOreNoBuff * bonus * intoProportion).toInt()
 
     println("Бонус кристаллической руды: $crystalOreBonus")
     println("Бонус железной руды: $ironOreBonus")
