b347190050aa4d214ccb010bdd4934f9b10879c9
annakizik <a.v.kizik@gmail.com>
2025-04-30 19:46:34 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson_3/L3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var greetings = "Добрый день"
+    val userName = "Анна"
+
+    println("$greetings, $userName")
+
+    greetings = "Добрый вечер"
+
+    println("$greetings, $userName")
+
+}
