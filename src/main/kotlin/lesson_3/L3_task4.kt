10737850e1a25d13f9a404a6267fdbdc3589914c
annakizik <a.v.kizik@gmail.com>
2025-04-30 21:00:07 +0300

    KS-3-4

---
+++ src/main/kotlin/lesson_3/L3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var fromWhere = "E2"
+    var toWhere = "E4"
+    var strokeNumber = 1
+
+    println("Ход №$strokeNumber: $fromWhere -> $toWhere")
+
+    fromWhere = "D2"
+    toWhere = "D3"
+    strokeNumber = 2
+
+    println("Ход №$strokeNumber: $fromWhere -> $toWhere")
+
+}
