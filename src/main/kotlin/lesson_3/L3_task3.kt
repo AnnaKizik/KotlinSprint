ff7f3f376a5c5074015c9fb2d8b87dbb8ed4852c
annakizik <a.v.kizik@gmail.com>
2025-05-02 13:07:01 +0300

    KS-3-3

--- src/main/kotlin/lesson_3/L3_task3.kt
+++ src/main/kotlin/lesson_3/L3_task3.kt
@@ -3,13 +3,7 @@ package org.example.lesson_3
 fun main() {
 
     val numberForMultiplicationTable = 5
-    val multiplicationIndex = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    for (i in multiplicationIndex) {
-        println(
-            """
-        |$numberForMultiplicationTable x $i = ${numberForMultiplicationTable * i}
-    """.trimMargin()
-        )
-    }
+    println((1..10).joinToString("\n") { "${numberForMultiplicationTable} x $it = ${numberForMultiplicationTable * it}" })
+
 }
