00b30a9acd7a3d5a8b89b8e821317e86c9170abc
annakizik <a.v.kizik@gmail.com>
2025-05-04 11:05:01 +0300

    KS-4-2

--- src/main/kotlin/lesson_4/L4_task2.kt
+++ src/main/kotlin/lesson_4/L4_task2.kt
@@ -3,8 +3,11 @@ package org.example.lesson_4
 fun main() {
 
     var cargoWeight = 20
+    val maxWeight = 100
+    val minWeight = 35
     var cargoVolume = 80
-    val averageParameters = cargoWeight > 35 && cargoWeight <= 100 && cargoVolume < 100
+    val maxVolume = 100
+    val averageParameters = cargoWeight > minWeight && cargoWeight <= maxWeight && cargoVolume < maxVolume
 
     println("Груз весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': $averageParameters")
 
