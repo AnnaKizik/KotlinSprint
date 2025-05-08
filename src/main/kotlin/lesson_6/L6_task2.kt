4c2af771d0b4d79ddf915208bebe688d175f9ee8
annakizik <a.v.kizik@gmail.com>
2025-05-08 19:08:18 +0300

    KS-6-2

---
+++ src/main/kotlin/lesson_6/L6_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_6
+
+fun main() {
+
+    print("На сколько секунд поставить таймер? – ")
+    var numberOfSeconds = readln().toInt()
+
+    var cycleCounter = 0
+
+    while (numberOfSeconds > 0) {
+        numberOfSeconds--
+        cycleCounter++
+        Thread.sleep(1000)
+    }
+
+    println("Прошло $cycleCounter секунд")
+
+}
