133e05d08ecf7f3a8c9336fe6dbcd564e78b00bd
anna_kizik <a.v.kizik@gmail.com>
2025-05-11 11:36:20 +0300

    KS-7-1 Диапазоны вне цикла

--- src/main/kotlin/lesson_7/L7_task1.kt
+++ src/main/kotlin/lesson_7/L7_task1.kt
@@ -2,16 +2,15 @@ package org.example.lesson_7
 
 fun main() {
 
-    var password = " "
+    var password = ""
+    val lettersForPassword = 'a'..'z'
+    val numbersForPassword = 0..9
 
     for (i in 1..6) {
-        if (i % 2 == 1) {
-            val letterForPassword = ('a'..'z').random()
-            password += letterForPassword
-        } else {
-            val numberForPassword = (0..9).random()
-            password += numberForPassword
-        }
+        if (i % 2 == 1)
+            password += lettersForPassword.random()
+        else
+            password += numbersForPassword.random()
     }
 
     println("Сгенерированный пароль: $password")
