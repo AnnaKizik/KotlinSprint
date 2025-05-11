981e9fa69fa5bbce8e0234be5abdf8dc09aa34c7
anna_kizik <a.v.kizik@gmail.com>
2025-05-11 13:49:37 +0300

    KS-8-1 Код упрощен

--- src/main/kotlin/lesson_8/L8_task1.kt
+++ src/main/kotlin/lesson_8/L8_task1.kt
@@ -3,12 +3,7 @@ package org.example.lesson_8
 fun main() {
 
     val viewsPerDay = arrayOf(34, 56, 67, 42, 12, 32, 66)
-    var weekViews = 0
 
-    for (i in viewsPerDay) {
-        weekViews = weekViews + i
-    }
-
-    println("Количество просмотров рекламы за неделю: $weekViews")
+    println("Количество просмотров рекламы за неделю: ${viewsPerDay.sum()}")
 
 }
