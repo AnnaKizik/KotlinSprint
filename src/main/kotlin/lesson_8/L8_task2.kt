c43569919473d6f5faebea1708ecc07d61526e2a
anna_kizik <a.v.kizik@gmail.com>
2025-05-11 13:55:10 +0300

    KS-8-2 Код упрощен

--- src/main/kotlin/lesson_8/L8_task2.kt
+++ src/main/kotlin/lesson_8/L8_task2.kt
@@ -5,15 +5,14 @@ fun main() {
     val ingredientsList = arrayOf("яйцо", "молоко", "мука", "масло", "корица", "соль", "сахар")
     print("Какой ингредиент найти? - ")
     val userIngredient = readln()
-    var presentIngredient = ""
 
     for (i in ingredientsList) {
-        if (userIngredient == i) presentIngredient += i
+        if (userIngredient in ingredientsList) {
+            println("Ингредиент $i в рецепте есть")
+            return
+        }
     }
 
-    if (presentIngredient.isNotBlank())
-        println("Ингредиент $presentIngredient в рецепте есть")
-    else
-        println("Такого ингредиента в рецепте нет")
+    println("Такого ингредиента в рецепте нет")
 
 }
