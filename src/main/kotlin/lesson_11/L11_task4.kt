c5a60bc44f02ca6cd389a43b110c01b2be77ca8d
anna_kizik <a.v.kizik@gmail.com>
2025-05-18 13:06:28 +0300

    KS-11-4 Исправлены недочеты

--- src/main/kotlin/lesson_11/L11_task4.kt
+++ src/main/kotlin/lesson_11/L11_task4.kt
@@ -1,20 +1,27 @@
 package org.example.lesson_11
 
+import java.net.URL
+
 class RecipeCategory(
-    val categoryImage: String,
+    val categoryId: Int,
+    val categoryImageUrl: String,
     val categoryName: String,
     val categoryDescription: String,
     val categoryRecipes: List<Recipe>,
 )
 
 class Recipe(
-    val recipeCategory: String,
+    val recipeId: Int,
+    val categoryId: Int,
+    val recipeImageUrl: String,
     val recipeName: String,
+    val cookingMethod: List<String>,
     val isInFavorites: Boolean,
     val recipeIngredients: List<Ingredient>,
 )
 
 class Ingredient(
     val ingredientName: String,
-    val forOnePortion: Int,
+    val quantity: Int,
+    val measurementUnits: String,
 )
