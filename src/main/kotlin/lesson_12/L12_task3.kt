19c16b2855e62a7a90743fc819b128d98c0a69f3
anna_kizik <a.v.kizik@gmail.com>
2025-05-20 20:30:31 +0300

    KS-12-3 Исправлены недочеты

--- src/main/kotlin/lesson_12/L12_task3.kt
+++ src/main/kotlin/lesson_12/L12_task3.kt
@@ -3,14 +3,9 @@ package org.example.lesson_12
 const val KELVIN_TO_CELSIUS = 273.15
 
 class Weather1(dayTemKelvin: Int, nightTemKelvin: Int, _downfall: Boolean) {
-    var dayTemperature = dayTemKelvin
-    var nightTemperature = nightTemKelvin
+    var dayTemperature = (dayTemKelvin - KELVIN_TO_CELSIUS).toInt()
+    var nightTemperature = (nightTemKelvin - KELVIN_TO_CELSIUS).toInt()
     val downfall = _downfall
-
-    init {
-        dayTemperature = (dayTemperature - KELVIN_TO_CELSIUS).toInt()
-        nightTemperature = (nightTemperature - KELVIN_TO_CELSIUS).toInt()
-    }
 
     fun weatherForecast() {
         println(
