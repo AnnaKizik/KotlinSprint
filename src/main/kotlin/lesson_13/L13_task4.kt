e270a0444ab36282b6e83451b0fb33d10684642f
anna_kizik <a.v.kizik@gmail.com>
2025-05-24 16:10:02 +0300

    KS-13-4

---
+++ src/main/kotlin/lesson_13/L13_task4.kt
@@ -0,0 +1,42 @@
+package org.example.lesson_13
+
+class ContactInfo(
+    val name: String,
+    val number: Long,
+    val company: String? = null
+) {
+    fun contactInformation() =
+        println("- Имя: $name\n- Номер: $number\n- Компания: ${company ?: "не указано"}\n")
+}
+
+fun main() {
+
+    val contactsList: MutableList<ContactInfo> = mutableListOf()
+    var contactName: String
+    var contactNumber: Long?
+    var contactCompany: String?
+    var newEntry = "нет"
+
+    do {
+        print("Введите имя контакта: ")
+        contactName = readln()
+
+        print("Введите номер контакта: ")
+        contactNumber = readln().toLongOrNull()
+        if (contactNumber == null) {
+            println("Вы не ввели номер!")
+            continue
+        }
+
+        print("Введите название компании: ")
+        contactCompany = readln()
+
+        contactsList.add(ContactInfo(contactName, contactNumber, contactCompany))
+
+        print("Добавить еще контакт (да/нет)? – ")
+        newEntry = readln()
+    } while (newEntry == "да")
+
+    contactsList.forEach { it.contactInformation() }
+
+}
