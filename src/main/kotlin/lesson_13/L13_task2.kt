51aa990135baa6bce48d77b35533e14766456977
anna_kizik <a.v.kizik@gmail.com>
2025-05-31 15:00:34 +0300

    KS-13-2 Исправлены недочеты

--- src/main/kotlin/lesson_13/L13_task2.kt
+++ src/main/kotlin/lesson_13/L13_task2.kt
@@ -3,13 +3,13 @@ package org.example.lesson_13
 class TelDirectory(
     val name: String,
     val number: Long,
-    val company: String?
-){
-    fun contactInformation()=
-        println("- Имя: $name\n- Номер: $number\n- Компания: ${company ?:"не указано"}")
+    val company: String? = null
+) {
+    fun printContactInformation() =
+        println("- Имя: $name\n- Номер: $number\n- Компания: ${company ?: "не указано"}")
 }
 
-fun main(){
-    val contact1 = TelDirectory("Андрей", 89657809954,null)
-    contact1.contactInformation()
+fun main() {
+    val contact1 = TelDirectory("Андрей", 89657809954, null)
+    contact1.printContactInformation()
 }
