99ca96a5d647e53cfbb7954a1fb1bd0dbaf48b43
anna_kizik <a.v.kizik@gmail.com>
2025-05-31 17:11:45 +0300

    KS-14-4 Исправлены недочеты

--- src/main/kotlin/lesson_14/L14_task4.kt
+++ src/main/kotlin/lesson_14/L14_task4.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_14
 
-abstract class CelestialBody(
+open class CelestialBody(
     val name: String,
     val hasAtmosphere: Boolean,
     val suitableForLanding: Boolean
@@ -10,8 +10,16 @@ open class Planet(
     name: String,
     hasAtmosphere: Boolean,
     suitableForLanding: Boolean,
-    val satellites: MutableList<Satellite> = mutableListOf()
-) : CelestialBody(name, hasAtmosphere, suitableForLanding)
+    var satellites: List<Satellite> = listOf()
+) : CelestialBody(name, hasAtmosphere, suitableForLanding) {
+
+    fun showInfoAboutSatellite() {
+        println("Планета: $name")
+        satellites.forEachIndexed { index, satellite ->
+            println("Спутник №${index + 1}: ${satellite.name}")
+        }
+    }
+}
 
 class Satellite(
     name: String,
@@ -21,16 +29,10 @@ class Satellite(
 
 fun main() {
 
-    val planet = Planet("Марс", false, true)
     val satellite1 = Satellite("Фобос", false, false)
     val satellite2 = Satellite("Деймос", false, false)
+    val planet = Planet("Марс", false, true, listOf(satellite1, satellite2))
 
-    planet.satellites.add(satellite1)
-    planet.satellites.add(satellite2)
-
-    println("Планета: ${planet.name}")
-    planet.satellites.forEachIndexed { index, satellite ->
-        println("Спутник №${index + 1}: ${satellite.name}")
-    }
+    planet.showInfoAboutSatellite()
 
 }
