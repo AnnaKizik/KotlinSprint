d57f6452e8b0d6161ecd56a67443f5f058ef600c
anna_kizik <a.v.kizik@gmail.com>
2025-06-05 20:45:45 +0300

    KS-16-4 Исправлены недочеты

--- src/main/kotlin/lesson_16/L16_task4.kt
+++ src/main/kotlin/lesson_16/L16_task4.kt
@@ -2,7 +2,7 @@ package org.example.lesson_16
 
 class Order(
     private val orderNumber: Int,
-    var status: String
+    private var status: String
 ) {
     private fun changeOrderStatus(newStatus: String) {
         status = newStatus
@@ -11,10 +11,12 @@ class Order(
     fun sendRequest(newStatus: String) {
         changeOrderStatus(newStatus)
     }
+
+    fun getStatus() = status
 }
 
 fun main() {
     val order = Order(23, "в пути")
     order.sendRequest("готов к выдаче")
-    println("Статус заказа изменен. Новый статус: ${order.status}")
+    println("Статус заказа изменен. Новый статус: ${order.getStatus()}")
 }
