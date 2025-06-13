0164d5c90c2cf46d4ede053da3bad77fcec6cd75
anna_kizik <a.v.kizik@gmail.com>
2025-06-13 15:52:09 +0300

    KS-18-2 Исправлены недочеты

--- src/main/kotlin/lesson_18/L18_task2.kt
+++ src/main/kotlin/lesson_18/L18_task2.kt
@@ -1,31 +1,18 @@
 package org.example.lesson_18
 
 const val DICE_MIN = 1
-const val MAX_FOR_D4 = 4
-const val MAX_FOR_D6 = 6
-const val MAX_FOR_D8 = 8
 
 abstract class Dice(private val numberOfFaces: Int) {
-    abstract fun rollDice()
-}
-
-class Dice4() : Dice(4) {
-    override fun rollDice() {
-        println("Результат броска (d4): ${(DICE_MIN..MAX_FOR_D4).random()}")
+    fun rollDice() {
+        println("Результат броска (d$numberOfFaces): ${(DICE_MIN..numberOfFaces).random()}")
     }
 }
 
-class Dice6() : Dice(6) {
-    override fun rollDice() {
-        println("Результат броска (d6): ${(DICE_MIN..MAX_FOR_D6).random()}")
-    }
-}
+class Dice4() : Dice(4)
 
-class Dice8() : Dice(8) {
-    override fun rollDice() {
-        println("Результат броска (d8): ${(DICE_MIN..MAX_FOR_D8).random()}")
-    }
-}
+class Dice6() : Dice(6)
+
+class Dice8() : Dice(8)
 
 fun main() {
     val diceList: List<Dice> = listOf(Dice4(), Dice6(), Dice8())
